2e766f0fb1a104056c6b53fae4db63bf19b589d3
Hao Mao <javabkb@gmail.com>
2019-03-20 02:03:30 +0000

    Change predict dir.

--- src/rml/constants.py
+++ src/rml/constants.py
@@ -2,7 +2,8 @@
 #DEST_DIR = '/Users/hmao/Documents/AI Stanford/Deep Learning/cs230/src/rml/data/cropped/'
 #DEST_DIR = '/home/ec2-user/cs230/src/rml/data/cropped/'
 DEST_DIR = '/home/ec2-user/data/cropped0/'
-TEST_DIR = '/home/ec2-user/testData/'
+#TEST_DIR = '/home/ec2-user/testData/'
+TEST_DIR = '/home/ec2-user/data/cropped0/hfDkDzZ9GsU/'
 LEARNING_RATE = 0.001
 LEARNING_RATE_STEP = 50
 LEARNING_RATE_GAMMA = 0.1
